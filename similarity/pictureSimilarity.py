80446d7796aec7e7b53032a9e8ff371d312bf9fd
zhujunda <11712504@mail.sustc.edu.cn>
2020-11-09 18:24:12 +0800

    [朱俊达] 使用list而非set

--- similarity/pictureSimilarity.py
+++ similarity/pictureSimilarity.py
@@ -17,26 +17,24 @@ def Hash(test_pic, pic_size, index_set):
     test_pic = cv2.resize(test_pic, (pic_size, pic_size))
     gray = cv2.cvtColor(test_pic, cv2.COLOR_BGR2GRAY)
     hash_str = ''
-    for i in range(pic_size):
-        for j in range(pic_size):
-            if (i, j) in index_set:
-                hash_str += str(int(gray[i, j] / 32))
+    for (i, j) in index_set:
+        hash_str += str(int(gray[i, j] / 32))
     return hash_str
 
 
 class HashCalculator:
-    valid_index_256 = set()
-    valid_index_32 = set()
+    valid_index_256 = []
+    valid_index_32 = []
 
     def __init__(self):
         for i in range(256):
             for j in range(256):
                 if (128 - i) * (128 - i) + (128 - j) * (128 - j) <= 128 * 128:
-                    self.valid_index_256.add((i, j))
+                    self.valid_index_256.append((i, j))
         for i in range(32):
             for j in range(32):
                 if (16 - i) * (16 - i) + (16 - j) * (16 - j) <= 16 * 16:
-                    self.valid_index_32.add((i, j))
+                    self.valid_index_32.append((i, j))
 
     def aHash(self, test_pic, pic_size):
         if pic_size == 32:
