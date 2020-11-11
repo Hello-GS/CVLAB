d2c7a5127f7c14dbc0114a77f40d73b3cd1f3aaa
zhujunda <11712504@mail.sustc.edu.cn>
2020-11-11 18:18:32 +0800

    [朱俊达] 定时任务，自动填写健康申报

---
+++ scheduled_task.py
@@ -0,0 +1,18 @@
+import time
+
+import requests
+import json
+url = "http://dailyhealth-api.sustech.edu.cn/api/form/save"
+data = {"sid": "11712504", "type": "1", "xm": "朱俊达", "deptName": "本科2017级", "curCity": "3", "dept": "ug2017",
+        "fanshenDate": "", "sfQz": "", "sfZx": "", "sfQgwh": "", "sfQghb": "", "jcs": "", "bxzz": "", "tiwen": "",
+        "local": "中国", "formDate": "2020-11-11", "nl": "20", "jtzz": "辽宁省-葫芦岛市-龙港区-锦葫路143-14号楼3单元36号", "xb": "男",
+        "mobile": "18820964977", "ylzd22": "14", "ylzd140": "0", "ylzd117": "下午六点 吃饭", "ylzd139": "0", "ylzd21": "36.7",
+        "ylzd116": "1", "ylzd23": "0"}
+header = {"accessToken": "a69c7f0c0543800bb025c735d64e6ac069800a70", "userId": "11712504",
+          "Content-Type": "application/json"}
+
+if __name__ == '__main__':
+    while True:
+        res = requests.post(url, data=json.dumps(data), headers=header)
+        print(res.text)
+        time.sleep(3600)
