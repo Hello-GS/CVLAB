382be3b661b7d4601183a25e5aa192023d2a3007
richzheng <11712501@mail.sustech.edu.cn>
2020-11-19 23:30:17 +0800

    [朱俊达] 瞎写点吧，随便

--- similarity/fenjiaodu.py
+++ similarity/fenjiaodu.py
@@ -1,26 +1,44 @@
 import configparser
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import requests
+from io import BytesIO
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+from similarity.testAccuracy import HashCalculator
 
-import yagmail
-
-user, password, receiver, host, result_path = '', '', '', '', ''
-
-
-def read_config():
-    config = configparser.ConfigParser()
-    config.read('./config.ini')
-    global user, password, receiver, host, result_path
-
-    user = config.get('email', 'user')
-    password = config.get('email', 'password')
-    receiver = config.get('email', 'receiver').split(',')
-    host = config.get('email', 'host')
-    result_path = config.get('data', 'result_path')
+input_path = '/disk/data/total_incident/'
+pic_length = 256
 
 
-def send_email(title, content, attachment):
-    yagmail.SMTP(user=user, password=password, host=host).send(receiver, title, content, attachment)
+def calculate_angle(i, j):
+    if i ==0 and j==0:
+        return 0
+    print(i,j)
+    x = i - 127
+    y = j - 127
+    if y < 0:
+        return 360 - math.acos(x / math.sqrt(x * x + y * y) / math.pi * 180)
+    elif y == 0:
+        if x > +0:
+            return 0
+        else:
+            return 180
 
+    return math.acos(x / math.sqrt(x * x + y * y) / math.pi * 180)
+
+
+def calculate_dest(i, j):
+    return abs(i - 127) * abs(i - 127) + abs(j - 127) * abs(j - 127)
+
+def calculateAngle():
+    for x in range(256):
+        for y in range(256):
+            calculate_angle(x,y)
 
 if __name__ == '__main__':
-    read_config()
-    send_email('附件形式发送计算结果', '','/disk/11712504/fuck/similarity/result_ans.txt')
+    calculate_angle(100,20)
+    calculateAngle()
