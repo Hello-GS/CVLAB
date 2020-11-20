31127c9861577c3892fc6824c1ebfce5bbf8cbb1
zhujunda <11712504@mail.sustc.edu.cn>
2020-11-20 14:42:47 +0800

    [朱俊达] 测试git

--- scheduled_task.py
+++ scheduled_task.py
@@ -8,7 +8,7 @@ data = {"sid": "11712504", "type": "1", "xm": "朱俊达", "deptName": "本科20
         "local": "中国", "formDate": "2020-11-11", "nl": "20", "jtzz": "辽宁省-葫芦岛市-龙港区-锦葫路143-14号楼3单元36号", "xb": "男",
         "mobile": "18820964977", "ylzd22": "14", "ylzd140": "0", "ylzd117": "下午六点 吃饭", "ylzd139": "0", "ylzd21": "36.7",
         "ylzd116": "1", "ylzd23": "0"}
-header = {"accessToken": "a69c7f0c0543800bb025c735d64e6ac069800a70", "userId": "11712504",
+header = {"accessToken": "2ea632204bc5670676504d870bef9a4e0f526094", "userId": "11712504",
           "Content-Type": "application/json"}
 
 if __name__ == '__main__':
